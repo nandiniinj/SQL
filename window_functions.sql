24a8567996c19a0ce847245c2e40a5904ac5530e
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-12-12 19:05:16 +0530

    Update window_functions.sql

--- window_functions.sql
+++ window_functions.sql
@@ -22,3 +22,37 @@ SELECT * FROM orders;
 select RANK() OVER(PARTITION BY customer_id ORDER BY order_amount DESC), order_id, customer_id, order_date, order_amount FROM orders_new;
 
 select RANK() OVER(PARTITION BY customer_id ORDER BY order_amount DESC) AS t, order_id, customer_id, order_date, order_amount FROM orders_new ORDER BY t, customer_id;
+
+
+
+
+
+
+
+
+
+CREATE TABLE employeess(
+  employee_id INT PRIMARY KEY,
+  employee_full_name VARCHAR(255) NOT NULL,
+  department VARCHAR(255) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL
+);
+
+INSERT INTO employeess VALUES
+(100, 'Mary Johns', 'SALES', 1000.00),
+(101, 'Sean Moldy', 'IT', 1500.00),
+(102, 'Peter Dugan', 'SALES', 2000.00),
+(103, 'Lilian Penn', 'SALES', 1700.00),
+(104, 'Milton Kowarsky', 'IT', 1800.00),
+(105, 'Mareen Bisset', 'ACCOUNTS', 1200.00),
+(106, 'Airton Graue', 'ACCOUNTS', 1100.00);
+
+SELECT
+  employee_id, 
+  employee_full_name, 
+  department,
+  salary,
+  ROUND(CAST(salary / MAX(salary) OVER (PARTITION BY department ORDER BY salary DESC)AS DECIMAL(10,2)),2)
+    AS salary_metric
+FROM employeess
+order by 5;
